ee0300765b8c76140ffab4654189e419606ea52d
AdamFarhan <adamfarhan11@gmail.com>
2018-10-05 16:58:32 -0400

    Finished data bindings, finished assignment 2

--- a2-bindings/src/app/app.module.ts
+++ a2-bindings/src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from'@angular/forms';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
+import { BodyComponent } from './body/body.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent
+    BodyComponent
   ],
   imports: [
     BrowserModule,
