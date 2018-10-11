b308f6af6d2f52a9f5940a6f77d555fb7a2054bc
Adam Farhan <adam.farhan@di-llc.com>
2018-10-11 12:36:56 -0400

    Added some angular Directives

--- s2-basics/src/app/servers/servers.component.ts
+++ s2-basics/src/app/servers/servers.component.ts
@@ -11,6 +11,7 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = "No server was created."
   serverName = '';
+  isServerCreated = false;
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
@@ -21,6 +22,7 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer(){
+    this.isServerCreated = true;
     this.serverCreationStatus = "Server was created.";
   }
 
