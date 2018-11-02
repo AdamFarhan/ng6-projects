e7c0f9e1bb0fb0d158f57ea1990446c5eb98a32b
AdamFarhan <adamfarhan11@gmail.com>
2018-11-01 23:11:57 -0400

    Created s3

--- s3-shopping/src/app/app.component.ts
+++ s3-shopping/src/app/app.component.ts
@@ -6,12 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'a3-directives';
-  items = [];
 
-  addItem(){
-    var tempItem = this.items.length + 1;
-    //this.items.push(tempItem);
-    this.items.push(new Date());
-  }
 }
