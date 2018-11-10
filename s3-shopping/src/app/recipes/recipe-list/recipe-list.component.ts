c1bf1b7fa0e507370bd2e197d0944a82e8a6f5ac
AdamFarhan <adamfarhan11@gmail.com>
2018-11-09 21:53:25 -0500

    Lecture 48 - Add Recipe Object, Build framework for recipe list

--- s3-shopping/src/app/recipes/recipe-list/recipe-list.component.ts
+++ s3-shopping/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+//import the Recipe Object from our typescript file
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  //create an array of Recipe objects
+  recipes: Recipe[] = [
+    new Recipe("A Test Recipe", "This is simply a test", "https://api.norecipes.com/wp-content/uploads/2018/08/teriyaki-chicken-recipe_007.jpg")
+  ];
   constructor() { }
 
   ngOnInit() {
