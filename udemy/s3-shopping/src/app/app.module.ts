3de4b1b6cc3710f62600226e5ce6129037fb22c9
Adam Farhan <adam.farhan@di-llc.com>
2019-04-18 16:03:23 -0400

    Up to episode 84

--- udemy/s3-shopping/src/app/app.module.ts
+++ udemy/s3-shopping/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
@@ -19,7 +19,8 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
     RecipeDetailComponent,
     RecipeItemComponent,
     ShoppingListComponent,
-    ShoppingEditComponent
+    ShoppingEditComponent,
+    FormsModule
   ],
   imports: [
     BrowserModule
--- udemy/s3-shopping/src/app/recipes/recipe-list/recipe-list.component.ts
+++ udemy/s3-shopping/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 //import the Recipe Object from our typescript file
 import { Recipe } from '../recipe.model';
@@ -12,10 +12,17 @@ export class RecipeListComponent implements OnInit {
   //create an array of Recipe objects
   //(name, description, imagePath)
   recipes: Recipe[] = [
-    new Recipe("A Test Recipe", "This is simply a test", "https://api.norecipes.com/wp-content/uploads/2018/08/teriyaki-chicken-recipe_007.jpg")
+    new Recipe("A Test Recipe", "This is simply a test", "https://www.tasteofhome.com/wp-content/uploads/2017/10/Healthier-than-Egg-Rolls_EXPS_SDON17_55166_C06_23_6b-696x696.jpg"),
+    new Recipe("Another Test Recipe", "This is simply a test", "https://www.tasteofhome.com/wp-content/uploads/2017/10/Healthier-than-Egg-Rolls_EXPS_SDON17_55166_C06_23_6b-696x696.jpg")
   ];
+
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
   }
 }
