4d0655a2a1b1181335b9a5863000ed6c3f7dd953
Adam Farhan <adam.farhan@di-llc.com>
2019-05-03 14:45:47 -0400

    Started a todo list thing

--- material/material-ui/src/app/customers/customers.module.ts
+++ material/material-ui/src/app/customers/customers.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CustomersRoutingModule } from './customers-routing.module';
 import { CustomerListComponent } from './customer-list/customer-list.component';
+import { CustomerCardComponent } from './customer-card/customer-card.component';
+import { CustomerNewComponent } from './customer-new/customer-new.component';
+
+//Material Components
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
-  declarations: [CustomerListComponent],
+  declarations: [CustomerListComponent, CustomerCardComponent, CustomerNewComponent],
   imports: [
     CommonModule,
-    CustomersRoutingModule
+    CustomersRoutingModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule
   ]
 })
 export class CustomersModule { }
